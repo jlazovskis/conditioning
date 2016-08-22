7dfabc0177fb622b231afad0fcc04a3338150b27
jlazovskis <jlazovskis@gmail.com>
2016-08-21 21:45:14 -0500

    major rework of surface con num finder. approach is now P3 -> A3 -> A2. needs more testing

--- surfaces.py
+++ surfaces.py
@@ -2,74 +2,76 @@ from __future__ import division
 # Name: Conditioning number program for surfaces
 # Description: Contains all the main functions for finding the conditioning number of a surface (that is, a variety defined by a single equation in P^3)
 # Created: 2016-08-19
-# Last modified: 2016-08-19
+# Last modified: 2016-08-21
 # Author: Janis Lazovskis
 
 # Import packages
-import numpy as np
+import math
 import sympy as sp
 
 # Declare variables, create a list of them, define input data class
-class thingy:
+class variety:
     def __init__(self):
         self.varlist = [] # variables to be used (e.g. [x0, x1, x2, x3] )
         self.points = []  # points to be tested (e.g. [[0,1,1,1],[1,0,1,1],[1,0,2,2]] )
         self.func = 0     # defining function (e.g. x0*x0*x1 - x2*x3*x3 +x0*x1*x2 + x2*x2*x2 )
-        self.jac = []     # Jacobian of function (always sp.Matrix([x.func]).jacobian(varlist) )
 
 # Import other files
 import helpers
 import curves
 
-def finder(data):
+# Conditioning number finder (projective, 4 coords)
+# (class variety) -> (list of real number)
+# If everything is fine with the input class, this function finds the conditioning number of the 
+# input class variety. If the surface is not smooth or the points are wrong, it returns an empty list
+# and prints a reason.
+def cnumsurface(data):
     # List of conditioning numbers
-    anslist = []
-    # Class to pass to curve finder
-    curvedata = thingy()
-    # Declare variables
-    for var in data.varlist:
-        var = sp.var(str(var))
-    nonzerocoords = []
-    for i in range(len(data.points)-2):
-        for j in range(4):
-            if data.points[i][j] != 0 and data.points[i+1][j] != 0 and data.points[i+2][j] != 0:
-                nonzerocoords.append(j)
-        if len(nonzerocoords) == 0:
-            print "Warning: Points "+str(i+1)+", "+str(i+2)+", "+str(i+3)+" do not lie in a common affine piece. Skipping them."
-        else:
-            for affpiece in nonzerocoords:
-                # New points projected to certain affine piece
-                npoints = []
-                for k in [0,1,2]:
-                    npoints.append(helpers.proj(data.points[i+k],affpiece))
-                # Matrix for finding equation of plane spanned by three points in npoints
-                M = sp.Matrix(( data.varlist[:affpiece]+data.varlist[affpiece+1:]+[1], npoints[0]+[1], npoints[1]+[1], npoints[2]+[1] ))
-                planeq = sp.det(M)
-                # Find some variable in equation to be replaced
-                curvarind = -1
-                sol = []
-                while sol == []:
-                    curvarind += 1
-                    sol = sp.solve(planeq, data.varlist[curvarind])
-                # Define new class for use by curve function
-                curvedata.varlist = data.varlist[:curvarind] + data.varlist[curvarind+1:]
-                curvedata.points = []
-                for l in [0,1,2]:
-                    curvedata.points.append(data.points[l][:curvarind] + data.points[l][curvarind+1:])
-                curvedata.func = data.func.subs({data.varlist[curvarind]:sol[0]})
-                curvedata.jac = sp.Matrix([curvedata.func]).jacobian(data.varlist[:curvarind] + data.varlist[curvarind+1:])
-                # Pass to curve finder
-                print curvedata.func
-                print curvedata.points
-                curves.checker(curvedata)
-                anslist  = anslist + curves.finder(curvedata)  
-        nonzerocoords = []
+    cnumlist = []
+    for p1 in data.points[:-2]:
+        for p2 in data.points[data.points.index(p1)+1:]:
+            for p3 in data.points[data.points.index(p2)+1:]:
+                # Check which affine piece(s) points lie in 
+                nonzerocoords = []
+                for i in range(4):
+                    if p1[i] != 0 and p2[i] != 0 and p3[i] != 0:
+                        nonzerocoords.append(i)
+                if len(nonzerocoords) != 0:
+                    for n in nonzerocoords:
+                        # Declare function and points in affine A^3 piece
+                        funca3 = data.func.subs({data.varlist[n]:1})
+                        pointsa3 = [helpers.proj(p1,n), helpers.proj(p2,n), helpers.proj(p3,n)]
+                        # Find equation of plane spanned by three points in A^3
+                        planeq = sp.det(sp.Matrix(( data.varlist[:n]+data.varlist[n+1:]+[1], pointsa3[0]+[1], pointsa3[1]+[1], pointsa3[2]+[1] )))
+                        # Choose first available variable to replace in funca3
+                        curvarind = -1
+                        sol = []
+                        while sol == [] and curvarind < 3:
+                            curvarind += 1
+                            sol = sp.solve(planeq, data.varlist[curvarind],dict=False)
+                        if sol != []:
+                            # Declare variety class to pass to conditioning number finder function for curves
+                            curvedata = variety()
+                            curvedata.func = funca3.subs({data.varlist[curvarind]:sol[0]})
+                            curvedata.points = pointsa3
+                            for p in curvedata.points:
+                                if n > curvarind:
+                                    del p[curvarind]
+                                else:
+                                    del p[curvarind-1]
+                            curvedata.varlist += data.varlist
+                            del curvedata.varlist[n]
+                            if n > curvarind:
+                                del curvedata.varlist[curvarind]
+                            else:
+                                del curvedata.varlist[curvarind-1]
+                            cnumlist.append(curves.cnumaff(curvedata,data.varlist[curvarind])) 
     # Print and return result
-    if anslist == []:
+    if cnumlist == []:
         print "No conditioning numbers found"
         return []
     else:
-        ans = min(anslist)
-        print str(len(anslist))+" conditioning numbers were found."
+        ans = min(cnumlist)
+        print str(len(cnumlist))+" conditioning numbers were found."
         print "The smallest of these is "+str(ans)+"."
         return [ans]
