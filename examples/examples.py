76f1c0a1057b0457c759b8084f80c682279a3d0b
jlazovskis <jlazovskis@gmail.com>
2016-08-29 09:10:25 -0500

    added new folder

---
+++ examples/examples.py
@@ -0,0 +1,47 @@
+# Name: Examples for using conditioning number finders for curves and surfaces
+# Description: Contains some examples with descriptions of how to use the functions
+# Created: 2016-08-18
+# Author: Janis Lazovskis
+
+# Navigate to the conditioning directory
+# Run Python 2
+
+# Example (curve)
+execfile('curves.py')
+x = variety()
+x0,x1,x2 = sp.var('x0,x1,x2')
+x.varlist = [x0,x1,x2]
+x.func = x0*x0 + x1*x2 - x1*x0
+x.points = [[1,1,0], [2,1,-2]]
+cnumcurve(x)
+
+# Non-example (curve)
+# Use the above, but instead, put:
+x.points = [[1,1,0], [2,1,-2], [0,0,0]]
+# Then cnumcurve will return an empty list saying the last point isn't in P^2
+cnumcurve(x)
+
+# Non-example (curve)
+# Use the above, but instead, put:
+x.points = [[1,1,0], [2,1,-2], [1,1,1]]
+# Then cnumcurve will return an empty list saying the last point isn't on the curve 
+cnumcurve(x)
+
+# Example surface
+execfile('surfaces.py')
+x = variety()
+x0,x1,x2,x3 = sp.var('x0,x1,x2,x3')
+x.varlist = [x0,x1,x2,x3]
+x.func = x0*x1 - x2*x3
+x.points = [[1,1,1,1], [0,1,1,0], [0,1,0,1], [2,1,1,2]]
+cnumsurface(x)
+
+# Non-example (surface)
+execfile('surfaces.py')
+x = variety()
+x0,x1,x2,x3 = sp.var('x0,x1,x2,x3')
+x.varlist = [x0,x1,x2,x3]
+x.func = x0*x0*x1 - x2*x3*x3 + x0*x1*x2 +x2*x2*x2
+x.points = [[0,1,1,1], [1,0,1,1], [1,0,2,2], [1,1,-1,1]]
+# This will raise an error because the curve is not smooth
+cnumsurface(x)
